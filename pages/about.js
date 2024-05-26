4cea7e2c6b32b07bfa40cb747e6bb50e88646124
Unknownleaf <94049800+Discobotdevlolhehe@users.noreply.github.com>
2024-05-26 19:19:10 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function About() {
         <div className="mt-8 max-w-3xl mx-auto p-4 bg-gray-800 rounded-lg shadow-lg">
           <h3 className="text-2xl mb-4">About Me</h3>
           <p className="mb-4">
-          Hello there, I am Unknown Leaf, a passionate web developer. My coding journey began in 2020 when I followed a tutorial to create a Discord bot. Since then, I&aposve built numerous bots and contributed to open-source projects. My first website was for a school project on agriculture, where I successfully demonstrated that a website could present details just as effectively as a PPT presentation.
+          Hello there, I am Unknown Leaf, a passionate web developer. My coding journey began in 2020 when I followed a tutorial to create a Discord bot. Since then, I&apos;ve built numerous bots and contributed to open-source projects. My first website was for a school project on agriculture, where I successfully demonstrated that a website could present details just as effectively as a PPT presentation.
           </p>
           <p className="mb-4">
             In my free time, I love to explore nature, hang out with my friends, and contribute to open-source projects. 
