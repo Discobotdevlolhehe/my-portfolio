25e56f4c4a1e40d14240947a2e9412329e626e5c
Unknownleaf <94049800+Discobotdevlolhehe@users.noreply.github.com>
2024-05-26 19:25:37 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,16 +6,16 @@ export default function About() {
     <Layout>
       <div className="text-center mt-16">
         <h2 className="text-4xl">
-          <Typing text={["Hello, I&apos;m Unknown Leaf"]} speed={100} eraseSpeed={50} />
+          <Typing text={["Hello, I am Unknown Leaf"]} speed={100} eraseSpeed={50} />
         </h2>
         <div className="mt-8 max-w-3xl mx-auto p-4 bg-gray-800 rounded-lg shadow-lg">
           <h3 className="text-2xl mb-4">About Me</h3>
           <p className="mb-4">
-          Hello there, I am Unknown Leaf, a passionate web developer. My coding journey began in 2020 when I followed a tutorial to create a Discord bot. Since then, I&apos;ve built numerous bots and contributed to open-source projects. My first website was for a school project on agriculture, where I successfully demonstrated that a website could present details just as effectively as a PPT presentation.
+          Hello there, I am Unknown Leaf, a passionate web developer. My coding journey began in 2020 when I followed a tutorial to create a Discord bot. Since then, I have built numerous bots and contributed to open-source projects. My first website was for a school project on agriculture, where I successfully demonstrated that a website could present details just as effectively as a PPT presentation.
           </p>
           <p className="mb-4">
             In my free time, I love to explore nature, hang out with my friends, and contribute to open-source projects. 
-            I believe in continuous growth and strive to be better every day. Let&apos;s connect and create something amazing together!
+            I believe in continuous growth and strive to be better every day. Let us connect and create something amazing together!
           </p>
         </div>
         <div className="mt-8 max-w-3xl mx-auto p-4 bg-gray-800 rounded-lg shadow-lg">
