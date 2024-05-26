95ebac6f3b3ed64a1d6ae71c856552d8525de0f9
Unknownleaf <94049800+Discobotdevlolhehe@users.noreply.github.com>
2024-05-26 19:19:28 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="bg-gray-800 p-4 mt-8">
         <div className="container mx-auto text-center text-white">
-          © 2024 Unknown Leaf&#39s Portfolio. All rights reserved.
+          © 2024 Unknown Leaf&apos;s Portfolio. All rights reserved.
         </div>
       </footer>
     );
